517a1dde738316c5f2e12f7d1dc05a48e3184bd7
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-05-03 12:24:36 +0600

    fix(cors): update allowed origin from 'https://car-phi-henna.vercel.app' to 'https://pandaasset.vercel.app'

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "https://asset-panda-rahul-khan-suvo.netlify.app",
-      "https://car-phi-henna.vercel.app",
+      "https://pandaasset.vercel.app",
       "http://localhost:5000",
       "http://localhost:5173",
     ],
